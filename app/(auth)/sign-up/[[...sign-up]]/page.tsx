bfff1a829caec5de188741c735dff0b29467d1c9
Mukund Rungta <mukundrungta143@gmail.com>
2026-08-25 23:04:30 +0530

    fix1

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,5 @@
 import { redirect } from "next/navigation";
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000";
-
 export default function Page() {
-  const url = `https://blessed-shrimp-45.accounts.dev/sign-up?redirect_url=${encodeURIComponent(
-    APP_URL,
-  )}`;
-  redirect(url);
+  redirect("https://blessed-shrimp-45.accounts.dev/sign-up");
 }
